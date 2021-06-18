2441a861d231aed02121750505d66daea53a4fbc
Ayman91 <ayman.mosstafa@gmail.com>
2021-06-18 02:10:02 +0200

    features section navigation functionaluty

--- JS/main.js
+++ JS/main.js
@@ -11,14 +11,23 @@ simple.addEventListener("click", () => {
   tab1.classList.remove("hide");
   tab2.classList.add("hide");
   tab3.classList.add("hide");
+  simple.classList.add("active");
+  speedy.classList.remove("active");
+  easy.classList.remove("active");
 });
 speedy.addEventListener("click", () => {
   tab1.classList.add("hide");
   tab2.classList.remove("hide");
   tab3.classList.add("hide");
+  simple.classList.remove("active");
+  speedy.classList.add("active");
+  easy.classList.remove("active");
 });
 easy.addEventListener("click", () => {
   tab1.classList.add("hide");
   tab2.classList.add("hide");
   tab3.classList.remove("hide");
+  simple.classList.remove("active");
+  speedy.classList.remove("active");
+  easy.classList.add("active");
 });
